decff54be89d13c4745615e777081b644b343231
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 17:09:19 +0300

    type-annotated python

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+    task 6
+"""
+
+
+def sum_mixed_list(mxd_lst: list[float, int]) -> float:
+    """
+    """
+
+    sum = 0.0
+
+    for num in mxd_lst:
+        sum += num
+
+    return sum
