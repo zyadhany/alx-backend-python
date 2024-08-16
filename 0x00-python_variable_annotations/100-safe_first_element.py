2fb02bb35b5a40a91d7df2b6e660921959f4a157
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 17:30:30 +0300

    type-annotated python

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    task 9
+"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    """
+
+    if lst:
+        return lst[0]
+    else:
+        return None
