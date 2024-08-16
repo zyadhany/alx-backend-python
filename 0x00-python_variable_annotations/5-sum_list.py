3a8cce6e8a7f24d1ed576041ec336de5cf2e5405
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 17:07:32 +0300

    type-annotated python

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+    task 5
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    """
+
+    sum = 0.0
+
+    for num in input_list:
+        sum += num
+
+    return sum
