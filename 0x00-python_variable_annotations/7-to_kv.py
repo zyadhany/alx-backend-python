c99b67b4d5eaf3bbe6c1a83741b46ae9575bff8c
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 17:16:13 +0300

    type-annotated python

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    task 7
+"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    """
+    return (k, v ** 2)
