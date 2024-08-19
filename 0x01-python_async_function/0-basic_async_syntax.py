910932584c1b9a7e19307f6422ffa045fe5522c2
zyadhany <zeyad.hany2003@gmail.com>
2024-08-20 01:01:19 +0300

    asycn python

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 
+    task 0
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ 
+    """
+    act: float = random.uniform(0, max_delay)
+    await asyncio.sleep(act)
+    return act
