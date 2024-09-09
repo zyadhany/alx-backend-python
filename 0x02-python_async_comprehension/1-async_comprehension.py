63009bd195ac1e9822e29a10e56e1b646fab0d5e
zyadhany <zeyad.hany2003@gmail.com>
2024-09-09 22:15:27 +0300

    asycn

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,9 @@
 
 import asyncio
 import random
-from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator():
     """
     """
     for i in range(10):
