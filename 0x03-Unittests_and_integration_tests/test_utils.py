4b32266a26b84b1da657e3e5d2ede3e84ede8e34
zyadhany <zeyad.hany2003@gmail.com>
2024-09-10 00:47:19 +0300

    unittest

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,14 +5,22 @@
 
 import unittest
 from utils import access_nested_map
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """TestAccessNestedMap class"""
 
-    def test_access_nested_map(self):
+    @parameterized.expand([
+        ({"a": 1}, ('a',), 1),
+        ({"a": {"b": 2}}, ('a',), {"b": 2}),
+        ({"a": {"b": 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, dic: Dict, path: Tuple[str],
+                               exp: Union[Dict, int]) -> None:
         """test_access_nested_map"""
-        self.assertEqual(access_nested_map({"a": 1}, ('a',)), 1)
+        self.assertEqual(access_nested_map(dic, path), exp)
 
 
 if __name__ == '__main__':
