505d25b422ba2a0b6eee453e10a9331654773f9e
zyadhany <zeyad.hany2003@gmail.com>
2024-09-10 02:02:59 +0300

    unittest

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,6 +32,20 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("google")
             self.assertEqual(client._public_repos_url, org["repos_url"])
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """ test_public_repos """
+        payload = [{"name": "google"}, {"name": "abc"}]
+        mock_json.return_value = payload
+
+        with patch('client.GithubOrgClient._public_repos_url') as mock_public_repos_url:
+            mock_public_repos_url.return_value = "hello"
+            client = GithubOrgClient("google")
+            res = client.public_repos()
+            self.assertEqual(res, ["google", "abc"])
+            mock_json.called_with_once()
+            mock_public_repos_url.called_with_once()
+
 
 if __name__ == '__main__':
     unittest.main()
